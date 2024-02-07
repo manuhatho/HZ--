c9471e3c7b04f3e35693d89307ee9564b6ec1c55
manuhatho <amateur9525@gmail.com>
2024-02-07 22:03:04 +0100

    l

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function menu(){
     const menu = document.querySelectorAll("#menu");
     const nav = document.querySelector(".nav2");
+    const header = document.querySelector("header");
   
     menu.forEach((index)=>{
         index.addEventListener("click", ()=>{
             index.classList.toggle("active");
             nav.classList.toggle("active");
+            header.classList.toggle("active");
          
         })
     })
