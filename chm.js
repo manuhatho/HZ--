3c63cc22a1c54ea58b9fa096658c4b870c315dab
manuhatho <amateur9525@gmail.com>
2024-03-05 15:11:54 +0100

    f

--- chm.js
+++ chm.js
@@ -1,57 +1,64 @@
-const carousel1 = document.querySelector(".carousel-1");
-const carousel2 = document.querySelector(".carousel-2");
-const carousel3 = document.querySelector(".carousel-3");
-const carousel4 = document.querySelector(".carousel-4");
-
-const arrowBtns = document.querySelectorAll('.card1');
-const arrowBtns2 = document.querySelectorAll('.card2');
-const arrowBtns3 = document.querySelectorAll('.card3');
-const arrowBtns4 = document.querySelectorAll('.card4');
 
 
-const firstDiapoWidth =  document.querySelector('.carousel-1').offsetWidth;
-const firstDiapoWidth2 =  document.querySelector('.carousel-2').offsetWidth;
-const firstDiapoWidth3 =  document.querySelector('.carousel-3').offsetWidth;
-const firstDiapoWidth4 =  document.querySelector('.carousel-4').offsetWidth;
+function scrollBtns(){
 
-
-arrowBtns.forEach(btn =>{
-
-   btn.addEventListener("click", ()=>{
+    const carousel1 = document.querySelector(".carousel-1");
+    const carousel2 = document.querySelector(".carousel-2");
+    const carousel3 = document.querySelector(".carousel-3");
+    const carousel4 = document.querySelector(".carousel-4");
     
-    console.log('click');
-    carousel1.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth;
-
-   })
-
-})
-arrowBtns2.forEach(btn =>{
-
-   btn.addEventListener("click", ()=>{
+    const arrowBtns = document.querySelectorAll('.card1');
+    const arrowBtns2 = document.querySelectorAll('.card2');
+    const arrowBtns3 = document.querySelectorAll('.card3');
+    const arrowBtns4 = document.querySelectorAll('.card4');
     
-    console.log('click');
-    carousel2.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth2;
-
-   })
-
-})
-arrowBtns3.forEach(btn =>{
-
-   btn.addEventListener("click", ()=>{
     
-    console.log('click');
-    carousel3.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth3;
-
-   })
-
-})
-arrowBtns4.forEach(btn =>{
-
-   btn.addEventListener("click", ()=>{
+    const firstDiapoWidth =  document.querySelector('.carousel-1').offsetWidth;
+    const firstDiapoWidth2 =  document.querySelector('.carousel-2').offsetWidth;
+    const firstDiapoWidth3 =  document.querySelector('.carousel-3').offsetWidth;
+    const firstDiapoWidth4 =  document.querySelector('.carousel-4').offsetWidth;
     
-    console.log('click');
-    carousel4.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth4;
-
-   })
-
-})
+    
+    arrowBtns.forEach(btn =>{
+    
+       btn.addEventListener("click", ()=>{
+        
+        console.log('click');
+        carousel1.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth;
+    
+       })
+    
+    })
+    arrowBtns2.forEach(btn =>{
+    
+       btn.addEventListener("click", ()=>{
+        
+        console.log('click');
+        carousel2.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth2;
+    
+       })
+    
+    })
+    arrowBtns3.forEach(btn =>{
+    
+       btn.addEventListener("click", ()=>{
+        
+        console.log('click');
+        carousel3.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth3;
+    
+       })
+    
+    })
+    arrowBtns4.forEach(btn =>{
+    
+       btn.addEventListener("click", ()=>{
+        
+        console.log('click');
+        carousel4.scrollLeft += btn.id === "previous" ? -firstDiapoWidth : firstDiapoWidth4;
+    
+       })
+    
+    })
+    
+    }
+    scrollBtns();
